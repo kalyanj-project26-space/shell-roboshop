c5d7c4cf90f235f8a8397b79fc9f16458d939b15
kalyan <kalyanjkr@gmail.com>
2026-02-09 11:49:14 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SG_ID="sg-076ec9ad23dab2b28" # replace with your ID
+SG_ID="sg-00edd68ec9e67bd52" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z05013202FKF0ZL12WAOP"
 DOMAIN_NAME="daws88s.online"
